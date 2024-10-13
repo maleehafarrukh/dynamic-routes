4f7654c7695f514c721dd94d0d1c51eb79750ec2
maleehafarrukh <159472134+maleehafarrukh@users.noreply.github.com>
2024-10-13 12:53:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import style from"./page.module.css";
+// import Image from "next/image";
+// import style from"./page.module.css";
 import Link from "next/link";
 
 export default function Home(){
